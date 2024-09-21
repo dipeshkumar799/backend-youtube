dbcc2bdf6bb5e71bab3c6dbdfdc5d804b0ca7c6d
dipeshkumar799 <yvdipesh0011@gmail.com>
2024-09-20 20:59:56 -0700

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,4 +1,14 @@
 import "dotenv/config";
 
 import connectDB from "./db/dbConnection.js";
-connectDB();
+import app from "./app.js";
+const PORT = process.env.PORT;
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB IS NOT CONNECTED", error);
+  });
