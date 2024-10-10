53d18c9953a177db30207c4d4f5411aabfe3bc4e
dipeshkumar799 <yvdipesh0011@gmail.com>
2024-10-10 10:10:25 -0700

    Resolved merge conflict in app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+<<<<<<< HEAD
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
@@ -10,4 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+=======
+const app = express();
+
+>>>>>>> dbcc2bdf6bb5e71bab3c6dbdfdc5d804b0ca7c6d
 export default app;
