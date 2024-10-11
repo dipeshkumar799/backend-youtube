9560e2208d1a22a5825c1f35ce94dbf71393c1b5
dipeshkumar799 <yvdipesh0011@gmail.com>
2024-10-11 12:42:59 -0700

    initial commit

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,44 +1,47 @@
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-    },
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            index: true,
+        },
 
-    email: {
-      type: String,
-      required: true,
-      unique: true,
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        fullname: {
+            type: string,
+            required: true,
+            index: true,
+        },
+        avatar: {
+            type: String, //cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String, //cloudinary url
+            required: true,
+        },
+        watchHistory: [
+          {
+             type: Schema.Types.ObjectId,
+            ref: "Video",
+        }
+      ],
+        refreshToken: {
+            type: String,
+        },
+        password: {
+            type: String,
+            required: [true,"password is required"]
+        },
     },
-    fullname: {
-      type: string,
-      required: true,
-      index: true,
-    },
-    avatar: {
-      type: String, //cloudinary url
-      required: true,
-    },
-    coverImage: {
-      type: String, //cloudinary url
-      required: true,
-    },
-    watchHistory: {
-      type: Schema.Types.ObjectId,
-      ref: "Video",
-    },
-    refreshToken: {
-      type: String,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 export const User = mongoose.model("User", userSchema);
--- src/models/Video.model.js
+++ src/models/Video.model.js
@@ -3,36 +3,37 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const videoSchema = new Schema(
-  {
-    videofile: {
-      // Cloudinary
-      type: String,
-      required: true,
+    {
+        videofile: {
+            // Cloudinary
+            type: String,
+            required: true,
+        },
+        thumbnail: {
+            type: String,
+            required: true,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+
+        title: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        view: {
+            type: Number,
+            required: true,
+        },
     },
-    thumbnail: {
-      type: String,
-      required: true,
-    },
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-    title: {
-      type: String,
-      required: true,
-    },
-    duration: {
-      type: Number,
-      required: true,
-    },
-    view: {
-      type: Number,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 export const Video = mongoose.model("Video", videoSchema);
